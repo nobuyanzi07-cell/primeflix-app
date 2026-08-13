f6f5c078d6f36eac783c373c6b020c98a00f668e
Nathan Oketch <nobuyanzi07@gmail.com>
2026-08-13 13:42:15 +0300

    Debbuging several files for Functionality

--- primeflix-app/src/pages/MovieDetails.jsx
+++ primeflix-app/src/pages/MovieDetails.jsx
@@ -1,10 +1,29 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getMovieDetails } from "../api/movieApi";
 import ScoreDial from "../components/ScoreDial";
 
-function MovieDetails({ movie }) {
+function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    async function fetchDetails() {
+      const data = await getMovieDetails(id);
+      setMovie(data);
+    }
+    fetchDetails();
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
+
   return (
     <div>
       <div>
-        <img src={movie.poster_path} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
 
         <div>
           <h1>{movie.title}</h1>
@@ -12,8 +31,8 @@ function MovieDetails({ movie }) {
           <ScoreDial score={movie.vote_average} />
 
           <p>{movie.release_date}</p>
-          <p>{movie.runtime}</p>
-          <p>{movie.genres}</p>
+          <p>{movie.runtime} min</p>
+          <p>{movie.genres?.map((g) => g.name).join(", ")}</p>
 
           <p>{movie.overview}</p>
         </div>
